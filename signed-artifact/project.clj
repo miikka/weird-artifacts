3edd5140a8ae6a66e9d188d49a7bf10f82d5a4ea
Miikka Koskinen <miikka@metosin.fi>
2020-07-27 20:33:43 +0300

    Initial commit

---
+++ signed-artifact/project.clj
@@ -0,0 +1,7 @@
+(defproject org.clojars.miikka/signed-artifact "0.1.0-SNAPSHOT"
+  :description "Signed with GPG"
+  :url "https://github.com/miikka/weird-artifacts"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :repl-options {:init-ns signed-artifact.core})
---
+++ signed-artifact/src/signed_artifact/core.clj
@@ -0,0 +1,6 @@
+(ns signed-artifact.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
