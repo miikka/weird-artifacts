858c278450b0465e0df912223720b70ab37e8024
Miikka Koskinen <miikka@metosin.fi>
2020-07-27 20:36:09 +0300

    Deploy releases to Clojars

--- signed-artifact/project.clj
+++ signed-artifact/project.clj
@@ -5,4 +5,5 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :repl-options {:init-ns signed-artifact.core}
+  :deploy-repositories [["releases" :clojars]]
   :vcs :git)
